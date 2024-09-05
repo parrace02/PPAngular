7160221792eb47524c64e7798305d5bf34057bce
Formacion <Formacion@bomberosfloridablanca.com>
2024-09-05 16:53:01 -0500

    mirando arreglar la imagen

--- Primer Proyecto con angular/PrimerProyecto/src/app/app.component.ts
+++ Primer Proyecto con angular/PrimerProyecto/src/app/app.component.ts
@@ -5,15 +5,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({  
   selector: 'app-root',  
   standalone: true,  
-  imports: [CommonModule, RouterOutlet], // Asegúrate de incluir CommonModule aquí  
+  imports: [CommonModule, RouterOutlet],  
   templateUrl: './app.component.html',  
   styleUrls: ['./app.component.css']  
 })  
 export class AppComponent {  
-  bienvenida = 'Hola';  
-  tasks: string[] = [  
-    'Instalar Angular',  
-    'Crear proyecto',  
-    'Crear componentes'  
-  ];  
+  // Aquí puedes agregar propiedades y métodos específicos para AppComponent  
 }
--- Primer Proyecto con angular/PrimerProyecto/src/app/pages/labs/labs.component.ts
+++ Primer Proyecto con angular/PrimerProyecto/src/app/pages/labs/labs.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  
+import { CommonModule } from '@angular/common'; // Importa CommonModule  
 
-@Component({
-  selector: 'app-labs',
-  standalone: true,
-  imports: [],
-  templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
-})
-export class LabsComponent {
-
+@Component({  
+  selector: 'app-labs',  
+  standalone: true,  
+  imports: [CommonModule], // Asegúrate de agregar CommonModule aquí  
+  templateUrl: './labs.component.html',  
+  styleUrls: ['./labs.component.css']  
+})  
+export class LabsComponent {  
+  bienvenida = 'Hola';  
+  tasks: string[] = [  
+    'Instalar Angular',  
+    'Crear proyecto',  
+    'Crear componentes'  
+  ];
+  Nombre = 'Andres';
+  Edad = 39;
+  disabled=true;
+  img = '246076832_4816627708348583_9102039506862301512_n.jpg';
 }
