933c4b16444925444e8ccbea8680c728abd99327
Formacion <Formacion@bomberosfloridablanca.com>
2024-09-18 10:48:12 -0500

    se elimino el app html y el css agrando un comando de angular se corrigio el error en el rauter

--- Primer Proyecto con angular/store/src/app/app.component.ts
+++ Primer Proyecto con angular/store/src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';  
+import { CommonModule } from '@angular/common';  
+import { RouterOutlet } from '@angular/router';  
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'store';
+@Component({  
+  selector: 'app-root',  
+  standalone: true,  
+  imports: [CommonModule, RouterOutlet],  
+  template: '<router-outlet></router-outlet>' // Cambiado a 'template'  
+})  
+export class AppComponent {  
+  title = 'store';  
 }
